024f5ce8dc966ee18636ecf351e0fee75086afa3
Emmanuel Buckshi <adventurist@gmail.com>
2023-12-22 20:06:30 -0500

    cleanup

--- js/search.js
+++ js/search.js
@@ -2,7 +2,7 @@ const fetch_exception = e => console.error('Error fetching search index')
 const render          = marked.marked
 const search_url      = '/search-index.json'
 const fuse_options    = { keys: ['title', 'content'],
-                          threshold: 0.2,
+                          threshold: 0.8,
                           caseSensitive: false }
 //-------------------------------------------------------------------
 //-------------------------------------------------------------------
@@ -15,50 +15,40 @@ document.addEventListener('DOMContentLoaded', async () =>
   if (!response || !response.ok)
     throw new Error("Failed to fetch search index")
 
-  const data = await response.json()
-  const fuse = new Fuse(data, fuse_options)
-  const do_search = query =>
-    {
-      if (!query) return
-
-      const results       = fuse.search(query)
-      container.innerHTML = ''
-
-      results.forEach(result =>
-      {
-        const content = document.createElement('div')
-        const li      = document.createElement('li')
-        const a       = document.createElement('a')
-        a.href        = result.item.url
-        li.innerHTML  = render(result.item.content)
-
-        a        .appendChild(document.createTextNode(result.item.title))
-        container.appendChild(a)
-        container.appendChild(li)
-      })
-    }
-
-    const delayed_search = debounce(do_search, 400)
-
-  //------------
-  input.addEventListener('input', async () =>
+  const data           = await response.json()
+  const fuse           = new Fuse(data, fuse_options)
+  const do_search      = query =>
   {
-    delayed_search(input.value.toLowerCase())
-  })
+    if (!query) return
+
+    const results       = fuse.search(query)
+    container.innerHTML = ''
+
+    results.forEach(result =>
+    {
+      const content = document.createElement('div')
+      const li      = document.createElement('li')
+      const a       = document.createElement('a')
+      a.href        = result.item.url
+      li.innerHTML  = render(result.item.content)
+
+      a        .appendChild(document.createTextNode(result.item.title))
+      container.appendChild(a)
+      container.appendChild(li)
+    })
+  }
+  const delayed_search = debounce(do_search, 400)
+  //------------
+  input.addEventListener('input', async () => { delayed_search(input.value.toLowerCase()) })
 })
 //-------------------------------------------------------------------
-function search (query, data)
-{
-  return new Fuse(data, { keys: ['title', 'content'] }).search(query)
-}
-//-------------------------------------------------------------------
-function debounce (func, delay)
+function debounce (fn, ms)
 {
   let timer
   return (...args) =>
   {
     if (timer)
       clearTimeout(timer)
-    timer = setTimeout(() => { func(...args) }, delay)
+    timer = setTimeout(() => { fn(...args) }, ms)
   }
 }
